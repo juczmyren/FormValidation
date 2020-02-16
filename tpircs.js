53943a568eb4739a84453ddecd70eb4836ad412d
Gabriel <gmjucs011@gmail.com>
2020-02-16 22:28:23 +0800

    first commit

---
+++ tpircs.js
@@ -0,0 +1,20 @@
+function sendReq(){
+    alert("Your event request has been sent");
+}
+
+function reset(){
+    prompt.alert("info has been reset");
+}
+
+function valiDATE(){
+    var start = new date(Edates);
+    var finish = new date(Edates2);
+
+    if(start>finish){
+        alert("goods");
+    }
+    else{
+        alert("Invalid date");
+    }
+
+}
